412094ac57bb908b16871d054ed162284665b98c
Jun <Jun@Junui-MacBook-Pro.local>
2017-10-18 19:38:41 +0900

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 import { NgModule } from '@angular/core';
+import {firebaseConfig} from "../environments/firebase.config";
+import {AngularFireModule} from 'angularfire2';
+import {RouterModule} from '@angular/router';
+import {routerConfig} from "./router.config";
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routerConfig),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/router.config.ts
@@ -0,0 +1,10 @@
+import { AppComponent } from './app.component';
+import {Routes} from '@angular/router';
+// import {HomeComponent} from "./home/home.component";
+
+export const routerConfig: Routes = [
+    {
+        path: 'home',
+        component: AppComponent
+    }
+];
